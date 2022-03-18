f6ed0c4fdf598265517fe9d2b4f535008fbb0ddf
adiosesr <Peru123456>
2022-03-17 23:06:48 -0500

    - Config exported true - Fill data to Recycler View

--- app/src/main/java/com/dioses/usersp/MainActivity.kt
+++ app/src/main/java/com/dioses/usersp/MainActivity.kt
@@ -17,13 +17,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        userAdapter = UserAdapter(mutableListOf())
+        userAdapter = UserAdapter(getUsers())
         linearLayoutManager = LinearLayoutManager(this)
 
         binding.recyclerView.apply {
             layoutManager = linearLayoutManager
             adapter = userAdapter
         }
+    }
 
+    private fun getUsers(): MutableList<User> {
+        val users = mutableListOf<User>()
+        val arthur = User(1, "Arthur", "Dioses", "https://frogames.es/wp-content/uploads/2020/09/alain-1.jpg")
+        val samanta = User(2, "Samanta", "Meza", "https://upload.wikimedia.org/wikipedia/commons/b/b2/Samanta_villar.jpg")
+        val wendy = User(3, "Wendy", "Castro", "https://upload.wikimedia.org/wikipedia/commons/d/d9/Emma_Wortelboer_%282018%29.jpg")
+        val alain = User(4, "Alain", "Nicolás", "https://frogames.es/wp-content/uploads/2020/09/alain-1.jpg")
+        val javier = User(5, "Javier", "Gómez", "https://live.staticflickr.com/974/42098804942_b9ce35b1c8_b.jpg")
+        val emma = User(6, "Emma", "Cruz", "https://upload.wikimedia.org/wikipedia/commons/d/d9/Emma_Wortelboer_%282018%29.jpg")
+
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(emma)
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(emma)
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(emma)
+        users.add(alain)
+        users.add(samanta)
+        users.add(javier)
+        users.add(emma)
+        users.add(arthur)
+        users.add(samanta)
+        users.add(wendy)
+
+        return users
     }
 }
